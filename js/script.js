8451a174ae9b4f8ffbbcd40326a46922851497ee
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-01-14 19:19:51 +0100

    up

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 arrayNum = numArrayGen(1, 100);
 randomArray = pickFromArray(arrayNum, 16);
+console.log(randomArray);
 
+// genera un array con numeri interi in sequenza da min a max.
 function numArrayGen(min, max) {
   var arrayNum = [];
   for (var i = min; i <= max; i++) {
@@ -8,6 +10,8 @@ function numArrayGen(min, max) {
   }
   return arrayNum;
 }
+
+// prende "num" numeri random pescati da array e li rimuove.
 function pickFromArray(array, num) {
   var randomArray = [];
   for (var i = 1; i <= num; i++) {
@@ -17,29 +21,46 @@ function pickFromArray(array, num) {
   }
   return randomArray;
 }
-console.log(randomArray);
 
-
-var trovato = false;
-var y = 0;
-do {
-  document.getElementById('gioca').addEventListener('click',
-  function() {
-    var NumeroScelto = document.getElementById('input').value;
+punteggio = 0;
+trovato = false;
+numeriVecchi = [];
+y = 0;
+document.getElementById('gioca').addEventListener('click',
+function() {
+  vecchio = false
+  NumeroScelto = document.getElementById('input').value;
+  var z = 0;
+  do {
+    if (NumeroScelto == numeriVecchi[z]) {
+      alert("numero già usato");
+      vecchio = true;
+    }
+    z++;
+  } while (vecchio == false && z < numeriVecchi.length);
+  document.getElementById('input').value = "";
+  if (vecchio == false) {
+    numeriVecchi.push(NumeroScelto);
     var j = 0;
     do {
-      if (NumeroScelto == randomArray[j] ) {
-         trovato = true;
+      if (NumeroScelto == randomArray[j]) {
+        trovato = true;
       }
       j++;
     } while (trovato == false && j < 16);
     y++;
     if (trovato == false) {
       document.getElementById('cella' + NumeroScelto).classList.add('green');
+      punteggio += 1;
+      document.getElementById('punteggio').innerHTML = "Punteggio: " + punteggio;
+    } else {
+      for (var i = 0; i < randomArray.length; i++) {
+        document.getElementById('cella' + randomArray[i]).classList.add('brown');
+      }
+      document.getElementById('cella' + NumeroScelto).classList.add('red');
     }
-  });  
-} while (trovato == false && y < 1);
-
-if (trovato == false) {
-  alert('hai vinto');
-}
+  }
+  if (trovato == false && y == 84) {
+    alert('hai vinto');
+  }
+});
