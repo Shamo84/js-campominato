ee81ead0d96783cbfb6876e3636f76aa11d33716
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-01-18 15:39:35 +0100

    up

--- js/script.js
+++ js/script.js
@@ -3,6 +3,76 @@ $(document).ready(function() {
   reset();
 
   $("#difficolta").change(function() {
+    sceltaDifficolta();
+  });
+
+
+  ////////////////////// tasto gioca
+  $(".container .cella").click(function() {
+    NumeroScelto = $(this).index()+1;
+    if (numeriVecchi.indexOf(NumeroScelto) == -1) {
+      numeriVecchi.push(NumeroScelto);
+      if (randomArray.indexOf(NumeroScelto) != -1) {
+        trovato = true;
+        for (var i = 0; i < randomArray.length; i++) {
+          document.getElementById('cella' + randomArray[i]).classList.add('brown');
+        }
+        document.getElementById('cella' + NumeroScelto).classList.add('red');
+        $("#overlay #testo p").text("Hai perso, ritenta!");
+        $("#overlay").show();
+      }
+      else {
+        document.getElementById('cella' + NumeroScelto).classList.add('green');
+        punteggio += 1;
+        document.getElementById('punteggio').innerHTML = "Punteggio: " + punteggio;
+      }
+    }
+    if (punteggio != 0) {
+      $("#difficolta").hide();
+    }
+    if (trovato == false && punteggio >= tentativi) {
+      $("#overlay #testo p").text("Hai vinto, complimenti!");
+      $("#overlay").show();
+    }
+  });
+
+  // TASTO OK
+  $("#ok").click(function() {
+    reset();
+  });
+
+  // genera un array con numeri interi in sequenza da min a max.
+  function numArrayGen(min, max) {
+    var arrayNum = [];
+    for (var i = min; i <= max; i++) {
+    arrayNum.push(i);
+    }
+    return arrayNum;
+  }
+
+  // prende "num" numeri random pescati da array e li rimuove.
+  function pickFromArray(array, num) {
+    var randomArray = [];
+    for (var i = 1; i <= num; i++) {
+      var randomNum = Math.floor(Math.random() * array.length);
+      randomArray.push(array[randomNum]);
+      array.splice(randomNum, 1);
+    }
+    return randomArray;
+  }
+
+  function reset() {
+    punteggio = 0;
+    trovato = false;
+    numeriVecchi = [];
+    sceltaDifficolta();
+    $("#overlay").hide();
+    $(".container > div").removeClass("green red brown");
+    document.getElementById('punteggio').innerHTML = "Punteggio: " + punteggio;
+    $("#difficolta").show();
+  }
+
+  function sceltaDifficolta() {
     difficolta = $('#difficolta').val();
     switch (difficolta) {
       case "difficile":
@@ -33,68 +103,5 @@ $(document).ready(function() {
       randomArray = pickFromArray(arrayNum, 16);
     }
     console.log(randomArray);
-  });
-
-
-  ////////////////////// tasto gioca
-  $(".container .cella").click(function() {
-    NumeroScelto = $(this).index()+1;
-    if (numeriVecchi.indexOf(NumeroScelto) == -1) {
-      numeriVecchi.push(NumeroScelto);
-      if (randomArray.indexOf(NumeroScelto) != -1) {
-        trovato = true;
-        for (var i = 0; i < randomArray.length; i++) {
-          document.getElementById('cella' + randomArray[i]).classList.add('brown');
-        }
-        document.getElementById('cella' + NumeroScelto).classList.add('red');
-        $("#overlay p").text("Hai perso, ritenta!");
-        $("#overlay").show();
-      }
-      else {
-        document.getElementById('cella' + NumeroScelto).classList.add('green');
-        punteggio += 1;
-        document.getElementById('punteggio').innerHTML = "Punteggio: " + punteggio;
-      }
-    }
-    if (trovato == false && punteggio >= tentativi) {
-      $("#overlay p").text("Hai vinto, complimenti!");
-      $("#overlay").show();
-    }
-  });
-
-  // TASTO OK
-  $("#ok").click(function() {
-    reset();
-    });
-  // genera un array con numeri interi in sequenza da min a max.
-  function numArrayGen(min, max) {
-    var arrayNum = [];
-    for (var i = min; i <= max; i++) {
-    arrayNum.push(i);
-    }
-    return arrayNum;
-  }
-
-  // prende "num" numeri random pescati da array e li rimuove.
-  function pickFromArray(array, num) {
-    var randomArray = [];
-    for (var i = 1; i <= num; i++) {
-      var randomNum = Math.floor(Math.random() * array.length);
-      randomArray.push(array[randomNum]);
-      array.splice(randomNum, 1);
-    }
-    return randomArray;
-  }
-
-  function reset() {
-    punteggio = 0;
-    trovato = false;
-    numeriVecchi = [];
-    arrayNum = numArrayGen(1, 100);
-    randomArray = pickFromArray(arrayNum, 16);
-    console.log(randomArray);
-    $("#overlay").hide();
-    $(".container > div").removeClass("green red brown");
-    document.getElementById('punteggio').innerHTML = "Punteggio: " + punteggio;
   }
 });
